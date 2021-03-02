6b290362768ecf41cb9baa5dd67919603bc2144c
David Castaneda <dcast188@fiu.edu>
2021-03-01 19:48:27 -0500

    (feat) update gray color in footer

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,7 +1,7 @@
 type Props = { endpoint?: string };
 export default function Footer({ endpoint }: Props) {
   return (
-    <footer className="py-8 border-t border-gray-300">
+    <footer className="py-8">
       <div className="text-center">
         <div className="flex flex-col">
           <p className="text-xs text-gray-400 font-medium">
@@ -9,7 +9,7 @@ export default function Footer({ endpoint }: Props) {
             <a
               href="https://krane.sh"
               target="_blank"
-              className="hover:text-gray-500"
+              className="hover:text-gray-700"
             >
               Krane
             </a>
@@ -19,7 +19,7 @@ export default function Footer({ endpoint }: Props) {
             <a
               href={`${endpoint}`}
               target="_blank"
-              className="text-xs text-gray-400 hover:text-gray-500 hover:cursor-pointer"
+              className="text-xs text-gray-400 hover:text-gray-700 hover:cursor-pointer"
             >
               {endpoint}
             </a>
