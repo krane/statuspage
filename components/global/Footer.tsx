4d07565448dcdcc6b6750f06d0b69b85ce2b0be3
David Castaneda <dcast188@fiu.edu>
2021-03-07 11:53:37 -0500

    (fix) Krane logo size

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="py-8 m-auto">
       <a href="https://krane.sh" target="_blank">
-        <KraneLogo width="7" height="7" />
+        <KraneLogo />
       </a>
     </footer>
   );
